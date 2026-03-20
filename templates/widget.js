43d924a3f2aae91e33f38b076f9281343bd0e9c8
gtpic <1301701321@qq.com>
2026-03-20 15:44:40 +0800

    Update widget.js

--- templates/widget.js
+++ templates/widget.js
@@ -67,8 +67,8 @@
         @media (max-width: 480px) { #cs-widget { transform-origin: bottom right; transform: scale(0.9); } }
         @media (max-width: 400px) { #cs-widget { transform-origin: bottom right; transform: scale(0.8); } }
         @media (max-width: 350px) { 
-            #cs-widget { transform-origin: bottom right; transform: scale(0.7); } 
-            #cs-panel { height: 430px; right: calc((50vw - 24px) / 0.7 - 180px); } 
+            #cs-widget { transform-origin: bottom right; transform: scale(0.85); } 
+            #cs-panel { height: 430px; right: calc((50vw - 24px) / 0.85 - 180px); } 
         }
     `;
     document.head.appendChild(style);
